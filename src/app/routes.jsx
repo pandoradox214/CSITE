1d145353a65c6ab5f5ff35cacdc71f7625e6d1c0
pandoradox214 <106655845+pandoradox214@users.noreply.github.com>
2026-03-10 11:43:18 +0800

    Update routes.jsx

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -9,7 +9,7 @@ import { EvaluationForm } from "./components/EvaluationForm.jsx";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/CSITE",
     Component: Layout,
     children: [
       { index: true, Component: About },
